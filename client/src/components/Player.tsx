58664b4c234de285acd28c99073ac81bf86e7e4c
Joyce Yuan <joyceyuan@Joyces-MBP.home>
2021-01-22 23:36:24 -0800

    some UI stuff

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../utilities.css";
 import Player from "../../../shared/Player";
+import './Player.css';
 
 // export interface Player {
 //     _id: String;
@@ -16,7 +17,7 @@ class PlayerBlock extends Component<Props> {
     }
     render() {
         return (
-            <div>
+            <div className = "circle" >
                 {this.props.player.name}
             </div>
         )
--- shared/Player.ts
+++ shared/Player.ts
@@ -3,6 +3,7 @@ import PlayerBlock from "../client/src/components/Player";
 export interface Player {
     _id: String;
     name: String;
+    color?: String;
 }
 
 export default Player;
