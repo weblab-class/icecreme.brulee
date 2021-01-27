e2257e90b578105bbe65bbb5c98414d86e8f07e0
William Xu <williamxu@Williams-MBP.home>
2021-01-27 10:04:39 -0500

    added create game functionality, need to modify css

--- client/src/components/pages/Join.tsx
+++ client/src/components/pages/Join.tsx
@@ -10,6 +10,7 @@ import Setup from "./Setup"
 import Player from "../../../../shared/Player";
 import {NewCodeInput, NewQuestionInput} from "../NewTextInput"
 
+
 const GOOGLE_CLIENT_ID = "1029457388024-o249v3ppd6up5tpigtvelkjsv3rgirj0.apps.googleusercontent.com";
 
 interface Props {
@@ -22,6 +23,21 @@ class Join extends Component<Props & RouteComponentProps, {}> {
         super(props);
     }
 
+    getRandomNumber = (min: number, max: number) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
+
+    generateCode = () => {
+        let newCode:string = '';
+        for (let i = 0; i < 4; i++) {
+            let char: string = String.fromCharCode(this.getRandomNumber(65, 91));
+            newCode+=char;
+        }
+        return newCode;
+    }
+
     render() {
         return (
             <>
@@ -30,7 +46,7 @@ class Join extends Component<Props & RouteComponentProps, {}> {
             </div>
             <div className = "rectangle2">
                 <div className = "circle1">
-                    <div className = "Create">
+                    <div className = "Create" onClick={()=>{this.props.setCode(this.generateCode())}}>
                         <h2 id="jointxt">Create game</h2>
                     </div>
                 </div>
