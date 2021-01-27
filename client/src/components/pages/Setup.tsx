739037d3bab28cef7281ac56ab83b581bdd68dff
Helen Yang <helenzy@mit.edu>
2021-01-26 21:25:35 -0500

    style changes

--- client/src/components/pages/Setup.tsx
+++ client/src/components/pages/Setup.tsx
@@ -9,17 +9,14 @@ import { TwitterPicker, PhotoshopPicker } from 'react-color';
 import { Button, Input, Grid} from 'semantic-ui-react'
 import { post } from '../../utilities';
 import { navigate } from "@reach/router";
-import Player from "../../../../shared/Player";
 
-interface Props {
-  player: Player
+
+
+type Props = {
+  userId: String;
+  handleLogin: (res: GoogleLoginResponse | GoogleLoginResponseOffline) => void;
+  handleLogout: () => void;
 }
-
-// type Props = {
-//   userId: String;
-//   handleLogin: (res: GoogleLoginResponse | GoogleLoginResponseOffline) => void;
-//   handleLogout: () => void;
-// }
 
 type State = {
   color: string;
@@ -42,6 +39,7 @@ class Setup extends Component<Props & RouteComponentProps, State> {
       const player = {name: this.state.name, color: this.state.color}
       console.log("new player attributes: " + player)
       post('/api/playerUpdate', player)
+      navigate("/game")
   }
   
   goToInfo = () => {
@@ -61,24 +59,28 @@ class Setup extends Component<Props & RouteComponentProps, State> {
     //   });
 
     return (
-      <>
-      <div className = 'avatar'>
-        <h1>Customize your avatar!</h1>
+      <div className = "setupContainer">
+          <input placeholder='Name' className = "center setupInput" onChange = {this.nameChange}></input>
+                    <div className = "newCircle center" style={{background:this.state.color}}>{this.state.name}</div>
+                    <TwitterPicker  className = "center" color={ this.state.color }
+                        //onChangeComplete={ this.onChangeColor}
+                        onSwatchHover = {this.onChangeColor}
+                    ></TwitterPicker>
+
+                <Button onClick = {this.handleSubmit}>Submit</Button>
+                <Button onClick = {this.goToInfo}>Game Info</Button>
+
+        {/* I think below are old code */}
+      {/* <div>
           <Input placeholder='Name' />
-      </div>
-
-      <div className = "newCircle" style={{background:this.state.color}}>{this.props.player.name}</div>
-
-      <div className = "palette">
+          <div className = "newCircle" style={{background:this.state.color}}>Hello</div>
           <TwitterPicker color={ this.state.color }
             onChangeComplete={ this.onChangeColor}
             onSwatchHover = {this.onChangeColor}></TwitterPicker>
-
             <Button onClick = {this.handleSubmit}>Submit</Button>
-
             <Button onClick = {this.goToInfo}>Game Info</Button>
-      </div>
-    </>
+      </div> */}
+    </div>
     )
   }
 }
