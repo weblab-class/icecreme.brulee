095ba115f06765027225d5a9359ead0e974cbfbe
Joyce Yuan <joyceyuan@Joyces-MBP.home>
2021-01-26 17:12:40 -0800

    new setup stuff

--- client/src/components/pages/Setup.tsx
+++ client/src/components/pages/Setup.tsx
@@ -5,7 +5,8 @@ import { IroColorPicker } from '@jaames/iro/dist/ColorPicker';
 const GOOGLE_CLIENT_ID = "1029457388024-o249v3ppd6up5tpigtvelkjsv3rgirj0.apps.googleusercontent.com";
 //const iro = require('@jaames/iro');
 import { TwitterPicker, PhotoshopPicker } from 'react-color';
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Input, Grid} from 'semantic-ui-react'
+import { post } from '../../utilities';
 
 
 
@@ -13,28 +14,29 @@ type Props = {
 }
 type State = {
   color: string;
+  name: string;
 }
 class Setup extends Component<Props & RouteComponentProps, State> {
   constructor(props){
     super(props);
     this.state = {
         color: "#0000ff",
+        name: "",
     }
   }
   onChangeColor = (color, event) => {
       console.log(color.hex)
       this.setState({color: color.hex})
   }
-  handleSubmit = () => {
-
+  handleSubmit = (event) => {
+      const player = {name: this.state.name, color: this.state.color}
+      console.log("new player attributes: " + player)
+      post('/api/playerUpdate', player)
   }
-//    componentDidMount() {
 
-//     colorPicker.on('color:change', function(color) {
-//         // log the current color as a HEX string
-//         this.setState({color: color.hexString})
-//       });
-//   }
+  nameChange = (event) => {
+      this.setState({name: event.target.value})
+  }
 
   render() {
     // let colorPicker = new iro.ColorPicker("#picker", {
@@ -45,14 +47,16 @@ class Setup extends Component<Props & RouteComponentProps, State> {
     //   });
 
     return (
-      <div>
-          <Input placeholder='Name' />
-          <div className = "newCircle" style={{background:this.state.color}}>Hello</div>
-          <TwitterPicker color={ this.state.color }
-            onChangeComplete={ this.onChangeColor}
-            onSwatchHover = {this.onChangeColor}></TwitterPicker>
+      <div className = "setupContainer">
+          <input placeholder='Name' className = "center setupInput" onChange = {this.nameChange}></input>
+                    <div className = "newCircle center" style={{background:this.state.color}}>{this.state.name}</div>
+                    <TwitterPicker  className = "center" color={ this.state.color }
+                        onChangeComplete={ this.onChangeColor}
+                        //onSwatchHover = {this.onChangeColor}
+                    ></TwitterPicker>
 
-            <Button onClick = {this.handleSubmit}>Submit</Button>
+                <button className = "center setupButton" onClick = {this.handleSubmit}>Submit</button>
+
       </div>
     )
   }
